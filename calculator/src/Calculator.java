e0d04ba0139ecfd48adadd5886f0d67ced674453
Nowcarpediem <94575562+Nowcarpediem@users.noreply.github.com>
2023-09-06 16:10:23 -0700

    Add files via upload

---
+++ calculator/src/Calculator.java
@@ -0,0 +1,191 @@
+// #1 imports
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+// #2 implements / extends
+public class Calculator implements ActionListener{
+    // #4 variable declarations
+    JFrame frame;
+    JTextField textField; // what is jtextfield?
+    JButton[] numberButtons = new JButton[10]; // JButton?
+    JButton[] functionButtons = new JButton[9];
+    JButton addButton, subButton, mulButton, divButton;
+    JButton decButton, equButton, delButton, clrButton, negButton;
+    JPanel panel; // JPanel?
+
+    Font myFont = new Font("Dialog", Font.BOLD, 30); // Font?
+
+    double num1 = 0, num2 = 0, result = 0;
+    char operator;
+
+    // #5 initialize the frame and textfield and buttons
+    Calculator() {
+
+        frame = new JFrame("Calculator");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(420, 550);
+        frame.setLayout(null);
+        
+        textField = new JTextField();
+        textField.setBounds(50, 25, 300, 50);
+        textField.setFont(myFont);
+        textField.setEditable(false);
+
+        addButton = new JButton("+");
+        subButton = new JButton("-");
+        mulButton = new JButton("*");
+        divButton = new JButton("/");
+        decButton = new JButton(".");
+        equButton = new JButton("=");
+        delButton = new JButton("Delete");
+        clrButton = new JButton("Clear");
+        negButton = new JButton("(-)");
+        // #6 add buttons to function array
+        functionButtons[0] = addButton;
+        functionButtons[1] = subButton;
+        functionButtons[2] = mulButton;
+        functionButtons[3] = divButton;
+        functionButtons[4] = decButton;
+        functionButtons[5] = equButton;
+        functionButtons[6] = delButton;
+        functionButtons[7] = clrButton;
+        functionButtons[8] = negButton;
+
+        // #7 create loop to set funciton button actionlistner and font
+        for (int i = 0; i < 9; i++) {
+            functionButtons[i].addActionListener(this);
+            functionButtons[i].setFont(myFont);
+            functionButtons[i].setFocusable(false); // get rid of outline
+        }
+
+        // #8 set number buttons
+
+        for (int i = 0; i < 10; i++) {
+            numberButtons[i] = new JButton(String.valueOf(i));
+            numberButtons[i].addActionListener(this);
+            numberButtons[i].setFont(myFont);
+            numberButtons[i].setFocusable(false); 
+        }
+        // #9 set del and clr buttons manually
+        negButton.setBounds(50, 430, 100, 50);
+        delButton.setBounds(150, 430, 100, 50);
+        clrButton.setBounds(250, 430, 100, 50);
+
+        // #10 create jpanel to hold all other buttons
+        panel = new JPanel();
+        panel.setBounds(50, 100, 300, 300);
+        panel.setLayout(new GridLayout(4, 4, 10, 10));
+        //panel.setBackground(Color.GRAY);
+        panel.add(numberButtons[1]);
+        panel.add(numberButtons[2]);
+        panel.add(numberButtons[3]);
+        panel.add(addButton);
+        panel.add(numberButtons[4]);
+        panel.add(numberButtons[5]);
+        panel.add(numberButtons[6]);
+        panel.add(subButton);
+        panel.add(numberButtons[7]);
+        panel.add(numberButtons[8]);
+        panel.add(numberButtons[9]);
+        panel.add(mulButton);
+        panel.add(decButton);
+        panel.add(numberButtons[0]);
+        panel.add(equButton);
+        panel.add(divButton);
+
+        //#9.5 add del and clr buttons to frame
+        frame.add(panel);
+        frame.add(delButton);
+        frame.add(clrButton);
+        frame.add(textField);
+        frame.setVisible(true);
+        frame.add(negButton);
+        
+    }
+
+    
+    // #11 set actions for buttons
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        
+        for (int i = 0; i < 10; i++) {
+            if (e.getSource() == numberButtons[i]) {
+                textField.setText(textField.getText().concat(String.valueOf(i)));
+            }
+        }
+
+        if (e.getSource() == decButton) {
+            textField.setText(textField.getText().concat("."));
+        }
+
+        if (e.getSource() == addButton) {
+            num1 = Double.parseDouble(textField.getText());
+            operator = '+';
+            textField.setText("");
+        }
+
+        if (e.getSource() == subButton) {
+            num1 = Double.parseDouble(textField.getText());
+            operator = '-';
+            textField.setText("");
+        }
+
+        if (e.getSource() == mulButton) {
+            num1 = Double.parseDouble(textField.getText());
+            operator = '*';
+            textField.setText("");
+        }
+
+        if (e.getSource() == divButton) {
+            num1 = Double.parseDouble(textField.getText());
+            operator = '/';
+            textField.setText("");
+        }
+
+        if (e.getSource() == equButton) {
+            num2 = Double.parseDouble(textField.getText());
+
+            switch(operator) {
+                case '+':
+                    result = num1 + num2;
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    break;
+                case '/':
+                    result = num1 / num2;
+                    break;
+            }
+
+            textField.setText(String.valueOf(result));
+            num1 = result;
+        }
+
+        if (e.getSource() == clrButton) {
+            textField.setText("");
+        }
+
+        if (e.getSource() == delButton) {
+            String string = textField.getText();
+            textField.setText("");
+
+            for (int i = 0; i < string.length()-1; i++) {
+                textField.setText(textField.getText()+string.charAt(i));
+            }
+        }
+
+        if (e.getSource() == negButton) {
+            double temp = Double.parseDouble(textField.getText());
+            temp = temp * -1;
+            textField.setText(String.valueOf(temp));
+        }
+    }
+
+    public static void main(String[] args) {
+        // #3 create instance of calculator class
+        Calculator calc = new Calculator();
+    }
+}
